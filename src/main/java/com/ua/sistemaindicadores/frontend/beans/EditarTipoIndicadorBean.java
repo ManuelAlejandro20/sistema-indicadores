9eb9bf55435f698151444646326527d6ed1e7653
ManuelAlejandro20 <mtrigo@adom.cl>
2022-05-19 03:01:49 -0400

    feat: añadido filtros responsivos

--- src/main/java/com/ua/sistemaindicadores/frontend/beans/EditarTipoIndicadorBean.java
+++ src/main/java/com/ua/sistemaindicadores/frontend/beans/EditarTipoIndicadorBean.java
@@ -6,6 +6,7 @@
 package com.ua.sistemaindicadores.frontend.beans;
 
 import com.ua.sistemaindicadores.backend.entities.IndicadorTipo;
+import com.ua.sistemaindicadores.backend.exceptions.NotificacionCorreoException;
 import com.ua.sistemaindicadores.backend.services.CorreoService;
 import com.ua.sistemaindicadores.backend.services.TipoIndicadorService;
 import java.io.IOException;
@@ -34,6 +35,7 @@ import javax.inject.Inject;
 public class EditarTipoIndicadorBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    private static final String direccionSI = "http://localhost:8080/SistemaIndicadores-1.0-SNAPSHOT/faces/inicio/inicio.xhtml";
     
     @Inject
     transient private TipoIndicadorService tipoIndicadorService;
@@ -101,9 +103,6 @@ public class EditarTipoIndicadorBean implements Serializable {
             context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_INFO, "ATENCIÓN", 
                     "El tipo de indicador " + nombreTipoIndicador + " ha sido actualizado correctamente")
             );
-            context.getExternalContext().getFlash().setKeepMessages(true);
-            context.getExternalContext()
-                    .redirect(context.getExternalContext().getRequestContextPath() + "/faces/administracion/admin-tipo-indicador.xhtml");
         
             try {
                 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
@@ -116,26 +115,35 @@ public class EditarTipoIndicadorBean implements Serializable {
                         + "<li>Fecha creación: " + formatter.format(it.getFechaCreacion()) + ".</li>"
                         + "<li>Fecha actualización: " + formatter.format(it.getFechaActualizacion()) + ".</li>"
                         + "</ul>"
+                        + "<br/><br/>"                                
+                        + "<a href=" + direccionSI + ">Link Sistema de Indicadores</a>"
+                        + "<br/><br/>"                                
                         + "<br/><br/>"
                         + "Saludos cordiales. <br/><br/>"
                         + "Sistema de Indicadores."
                 );   
-            } catch (Exception ex) {
+            } catch (NotificacionCorreoException ex) {
                 
                 //En caso de que ocurra una excepcion el registro en la BD no se actualiza pero el objeto tipo de indicador si
                 //lo hace, por lo que se tiene que volver a setear los valores a los que tenia antes de actualizar
                 it.setNombre(nombreAntiguo);
                 it.setEstado(vigenciaAntiguaSh);
-                it.setDescripcion(descripcionAntigua);                
+                it.setDescripcion(descripcionAntigua);      
+                
+                context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_WARN, "ATENCIÓN", 
+                        "Ocurrio un error al enviar el correo. Contacte al administrador mediante el correo SOPORTE.DVCME@uantof.cl.")
+                );                        
                 
                 //En caso de capturar algun error se retorna un mensaje y se guarda en el log el error
                 Logger.getLogger(CrearTipoIndicadorBean.class
                         .getName()).log(Level.SEVERE, "Ocurrio un error al enviar el correo.", ex);
-                FacesContext
-                        .getCurrentInstance()
-                        .addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Ocurrio un error al enviar el correo. Contacte al administrador mediante el correo SOPORTE.DVCME@uantof.cl.")
-                        );
-            }             
+
+            }      
+            
+            context.getExternalContext().getFlash().setKeepMessages(true);
+            context.getExternalContext()
+                    .redirect(context.getExternalContext().getRequestContextPath() + "/faces/administracion/admin-tipo-indicador.xhtml");     
+            
         }
         catch(EJBException e){
             
