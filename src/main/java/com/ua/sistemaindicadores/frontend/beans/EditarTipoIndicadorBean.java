341767053d17c284975ea0c1e91f63ee7238f00a
ManuelAlejandro20 <mtrigo@adom.cl>
2022-05-28 15:56:02 -0400

    feat: añadida nueva forma de enviar parametros a la interfaz editar tipo indicador, nuevas comprobaciones añadidas

--- src/main/java/com/ua/sistemaindicadores/frontend/beans/EditarTipoIndicadorBean.java
+++ src/main/java/com/ua/sistemaindicadores/frontend/beans/EditarTipoIndicadorBean.java
@@ -17,6 +17,7 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;  
+import java.util.Map;
 import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -43,7 +44,6 @@ public class EditarTipoIndicadorBean implements Serializable {
     transient private CorreoService correoService;  
     
     private IndicadorTipo it;
-    private String id; 
     private String nombreTipoIndicador; 
     private String vigencia;
     private String descripcion;    
@@ -62,114 +62,116 @@ public class EditarTipoIndicadorBean implements Serializable {
     public EditarTipoIndicadorBean() {
     }
 
-    public void cargarDatos(){ 
-        if(id != null){       
-            it = tipoIndicadorService.buscarTipoIndicadorID(Integer.valueOf(id));
-            nombreTipoIndicador = it.getNombre();
-            descripcion = it.getDescripcion();
-
-            if(it.getEstado() == 1){
-                vigencia = "VIGENTE";
-            }else{
-                vigencia = "NO VIGENTE";
-            }
-        }
+    public void cargarDatos(){     
         
+        Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String id = params.get("i");
+ 
+        //El id es nulo cuando cargamos la pagina pero no brindamos ningun parametro
+        if(id != null){
+            it = tipoIndicadorService.buscarTipoIndicadorID(Integer.valueOf(id));
+        
+            //it es nulo cuando el id leido no existe en la bd
+            if(it != null){
+                nombreTipoIndicador = it.getNombre();
+                descripcion = it.getDescripcion();
+
+                if(it.getEstado() == 1){
+                    vigencia = "VIGENTE";
+                }else{
+                    vigencia = "NO VIGENTE";
+                }
+            }            
+        
+        }                       
     }
     
     public void actualizarDatos() throws IOException{        
-        
-        short numVigencia = 0; 
-        if(vigencia.equals("VIGENTE")){
-            numVigencia = 1;
-        }
-        
-        //Datos del tipo de indicador antes de actualizar
-        String nombreAntiguo = it.getNombre();
-        Short vigenciaAntiguaSh = it.getEstado();
-        String vigenciaAntigua = (it.getEstado() == 1)? "VIGENTE" : "NO VIGENTE";
-        String descripcionAntigua = it.getDescripcion();
-        
-        FacesContext context = FacesContext.getCurrentInstance();
-        
-        //Se actualizan los datos del tipo indicador
-        it.setNombre(nombreTipoIndicador);
-        it.setEstado(numVigencia);
-        it.setDescripcion(descripcion);
-        it.setFechaActualizacion(new Date());
-        
-        try{
-            tipoIndicadorService.actualizarTipoIndicador(it);          
-            context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_INFO, "ATENCIÓN", 
-                    "El tipo de indicador " + nombreTipoIndicador + " ha sido actualizado correctamente")
-            );
-        
-            try {
-                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                formatter.setTimeZone(TimeZone.getTimeZone("GMT-4"));
-                correoService.enviarMensajeTexto("manueltrigo.at@gmail.com", "Sistema de Indicadores", "Se ha actualizado un tipo de indicador.<br/> "
-                        + "<ul>"
-                        + "<li>Nombre tipo de indicador: " + nombreAntiguo + " ---> " + it.getNombre() + ".</li>"
-                        + "<li>Estado: " + vigenciaAntigua + " ---> " + vigencia + ".</li>"
-                        + "<li>Descripción: " + descripcionAntigua + " ---> " + it.getDescripcion() + ".</li>"
-                        + "<li>Fecha creación: " + formatter.format(it.getFechaCreacion()) + ".</li>"
-                        + "<li>Fecha actualización: " + formatter.format(it.getFechaActualizacion()) + ".</li>"
-                        + "</ul>"
-                        + "<br/><br/>"                                
-                        + "<a href=" + direccionSI + ">Link Sistema de Indicadores</a>"
-                        + "<br/><br/>"                                
-                        + "<br/><br/>"
-                        + "Saludos cordiales. <br/><br/>"
-                        + "Sistema de Indicadores."
-                );   
-            } catch (NotificacionCorreoException ex) {
-                
+        //El it es nulo cuando damos por parametro una id que no existe o id nulo
+        if(it != null){
+            short numVigencia = 0; 
+            if(vigencia.equals("VIGENTE")){
+                numVigencia = 1;
+            }
+
+            //Datos del tipo de indicador antes de actualizar
+            String nombreAntiguo = it.getNombre();
+            Short vigenciaAntiguaSh = it.getEstado();
+            String vigenciaAntigua = (it.getEstado() == 1)? "VIGENTE" : "NO VIGENTE";
+            String descripcionAntigua = it.getDescripcion();
+
+            FacesContext context = FacesContext.getCurrentInstance();
+
+            //Se actualizan los datos del tipo indicador
+            it.setNombre(nombreTipoIndicador);
+            it.setEstado(numVigencia);
+            it.setDescripcion(descripcion);
+            it.setFechaActualizacion(new Date());
+
+            try{
+                tipoIndicadorService.actualizarTipoIndicador(it);          
+                context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_INFO, "ATENCIÓN", 
+                        "El tipo de indicador " + nombreTipoIndicador + " ha sido actualizado correctamente")
+                );
+
+                try {
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    formatter.setTimeZone(TimeZone.getTimeZone("GMT-4"));
+                    correoService.enviarMensajeTexto("manueltrigo.at@gmail.com", "Sistema de Indicadores", "Se ha actualizado un tipo de indicador.<br/> "
+                            + "<ul>"
+                            + "<li>Nombre tipo de indicador: " + nombreAntiguo + " ---> " + it.getNombre() + ".</li>"
+                            + "<li>Estado: " + vigenciaAntigua + " ---> " + vigencia + ".</li>"
+                            + "<li>Descripción: " + descripcionAntigua + " ---> " + it.getDescripcion() + ".</li>"
+                            + "<li>Fecha creación: " + formatter.format(it.getFechaCreacion()) + ".</li>"
+                            + "<li>Fecha actualización: " + formatter.format(it.getFechaActualizacion()) + ".</li>"
+                            + "</ul>"
+                            + "<br/><br/>"                                
+                            + "<a href=" + direccionSI + ">Link Sistema de Indicadores</a>"
+                            + "<br/><br/>"                                
+                            + "<br/><br/>"
+                            + "Saludos cordiales. <br/><br/>"
+                            + "Sistema de Indicadores."
+                    );   
+                } catch (NotificacionCorreoException ex) {
+
+                    //En caso de que ocurra una excepcion el registro en la BD no se actualiza pero el objeto tipo de indicador si
+                    //lo hace, por lo que se tiene que volver a setear los valores a los que tenia antes de actualizar
+                    it.setNombre(nombreAntiguo);
+                    it.setEstado(vigenciaAntiguaSh);
+                    it.setDescripcion(descripcionAntigua);      
+
+                    context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_WARN, "ATENCIÓN", 
+                            "Ocurrio un error al enviar el correo. Contacte al administrador mediante el correo SOPORTE.DVCME@uantof.cl.")
+                    );                        
+
+                    //En caso de capturar algun error se retorna un mensaje y se guarda en el log el error
+                    Logger.getLogger(CrearTipoIndicadorBean.class
+                            .getName()).log(Level.SEVERE, "Ocurrio un error al enviar el correo.", ex);
+
+                }      
+
+                context.getExternalContext().getFlash().setKeepMessages(true);
+                context.getExternalContext()
+                        .redirect(context.getExternalContext().getRequestContextPath() + "/faces/administracion/admin-tipo-indicador.xhtml");     
+
+            }
+            catch(EJBException e){
+
                 //En caso de que ocurra una excepcion el registro en la BD no se actualiza pero el objeto tipo de indicador si
-                //lo hace, por lo que se tiene que volver a setear los valores a los que tenia antes de actualizar
+                //lo hace, por lo que se tiene que volver a setear los valores a los que tenia antes de actualizar            
                 it.setNombre(nombreAntiguo);
                 it.setEstado(vigenciaAntiguaSh);
-                it.setDescripcion(descripcionAntigua);      
-                
-                context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_WARN, "ATENCIÓN", 
-                        "Ocurrio un error al enviar el correo. Contacte al administrador mediante el correo SOPORTE.DVCME@uantof.cl.")
-                );                        
-                
-                //En caso de capturar algun error se retorna un mensaje y se guarda en el log el error
-                Logger.getLogger(CrearTipoIndicadorBean.class
-                        .getName()).log(Level.SEVERE, "Ocurrio un error al enviar el correo.", ex);
+                it.setDescripcion(descripcionAntigua);
 
-            }      
-            
-            context.getExternalContext().getFlash().setKeepMessages(true);
-            context.getExternalContext()
-                    .redirect(context.getExternalContext().getRequestContextPath() + "/faces/administracion/admin-tipo-indicador.xhtml");     
-            
+                context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ATENCIÓN", 
+                        "El tipo de indicador " + nombreTipoIndicador + " ya existe en los registros")
+                );            
+
+
+            }          
         }
-        catch(EJBException e){
-            
-            //En caso de que ocurra una excepcion el registro en la BD no se actualiza pero el objeto tipo de indicador si
-            //lo hace, por lo que se tiene que volver a setear los valores a los que tenia antes de actualizar            
-            it.setNombre(nombreAntiguo);
-            it.setEstado(vigenciaAntiguaSh);
-            it.setDescripcion(descripcionAntigua);
-            
-            context.addMessage("mensaje", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ATENCIÓN", 
-                    "El tipo de indicador " + nombreTipoIndicador + " ya existe en los registros")
-            );            
-            
-            
-        }          
-        
     }    
     
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getNombreTipoIndicador() {
         return nombreTipoIndicador;
     }
